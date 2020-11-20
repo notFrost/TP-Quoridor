91253aaa774755a65da31cafb3e2688a74319788
Diego Pereira <38121350+notFrost@users.noreply.github.com>
2020-11-20 12:30:13 -0500

    Minor changes for Git utilization

--- TPQuoridor/TPQuoridor/TPQuoridor.py
+++ TPQuoridor/TPQuoridor/TPQuoridor.py
@@ -203,7 +203,7 @@ def Astar(expanded, viewed, walls, end):
   if viewed:
     return Astar(expanded, viewed, walls, end)
   else:
-    return false
+    return False
  
 #BFS ALGORITHM
 def BFS (expanded, viewed, walls, end):
