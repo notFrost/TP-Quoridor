be54701c18bfdbfdb2044cda829a8a967da54490
Diegolivia <diegolivia@hotmail.com>
2020-11-24 07:02:21 -0500

    Finalizado generacion y manejo de paredes

--- TPQuoridor/TPQuoridor/GameEngine.py
+++ TPQuoridor/TPQuoridor/GameEngine.py
@@ -14,15 +14,14 @@ class Engine:
         self.imgBank = []
         self.SpriteBank = []
         self.SpritePlayer = []
-        self.SpriteWalls=[]
         self.ArrPlayer = []
         self.ArrPathing = []
-        self.viewed, self.expanded,self.walls = [],[],[]
-        self.wall_array=False
+        self.viewed, self.expanded= [],[]
+        self.walls=False
+        self.mode=0
 
         self.load_env()
         self.generate_players(NUMPLAYERS)
-        #self.draw_players()
         self.ShowPath()
 
     def load_env(self):
@@ -43,11 +42,16 @@ class Engine:
                                             )
                 Slot.scale=SLOT_SCALE
                 self.SpriteBank.append(Slot)
-        self.wall_array=WallArray(self.imgBank[2],self.imgBank[3])
+        self.walls=WallArray(self.imgBank[2],self.imgBank[3])
         
     def generate_players(self,Nplay):
-        #Posiciones iniciales de los 4 Jugadores posibles
-        #En orden van 0->Arriba 1->Abajo 2->Izquierda 3->Derecha
+        """
+       Posiciones iniciales de los 4 Jugadores posibles:
+       0->Arriba
+       1->Arriba,Abajo 
+       2->Arriba,Abajo,Izquierda 
+       3->Arriba,Abajo,Izquierda,Derecha
+        """
         INITIAL_LOCATIONS = [(int(BOARD_SIZE/2) +1 ,  0),
                              (int(BOARD_SIZE/2) -1   ,  BOARD_SIZE-1),
                              (0                 ,  int(BOARD_SIZE/2)    +1) ,
@@ -62,23 +66,37 @@ class Engine:
 
     def ShowPath(self):
         for x in self.ArrPlayer:
-            x.generate_route(self.expanded,self.viewed,self.walls)
+            x.generate_route(self.expanded,self.viewed,self.walls.wall_array)
             x.show_route()
-            self.viewed, self.expanded,self.walls = [],[],[]
+            self.viewed, self.expanded = [],[]
         pass
 
-    def key_command(self,key):
-        if key==1:
-            self.wall_array.show_shadow_wall()
-        elif key==2:
-            self.wall_array.rotate_wall()
-        elif key==3:
-            self.PlaceWall()
 
+    def set_mode(self,key):
+        """
+        DESIGNACION DE LOS MODOS DEL JUEGO
+        0  -> Idle, los jugadores pueden clickear el entorno
+        1  -> Wall_Placement, un jugador va a colocar una pared
+        2  -> Player Move, un jugador va a mover su ficha
+        """
+        self.mode=key
+
+    def key_command(self,key):
+        if self.mode==0:
+            if key==1:
+                self.set_mode(1)
+                self.walls.command_manager(key)
+        elif self.mode==1:
+            if key==3 or key==2:
+                if key==3:
+                    self.set_mode(0)
+                self.walls.command_manager(key)
+    
     def PlaceWall(self):
         print("se coloco xd")
 
     def TrackMouse(self,Mx,My):
-        self.wall_array.refresh_shadow_pos(Mx,My)
+        if self.mode==1:
+            self.walls.refresh_shadow_pos(Mx,My)
         pass
  
--- TPQuoridor/TPQuoridor/InteractableClasses.py
+++ TPQuoridor/TPQuoridor/InteractableClasses.py
@@ -2,11 +2,13 @@ from Pathfinders import *
 
 #Configurar Engine Renderer
 Bruh=pyglet.graphics.Batch()
-Background = pyglet.graphics.OrderedGroup(0)
-BoardObj = pyglet.graphics.OrderedGroup(1)
-Foreground = pyglet.graphics.OrderedGroup(2)
-Walls = pyglet.graphics.OrderedGroup(4)
-Players = pyglet.graphics.OrderedGroup(3)
+OutOfBounds=pyglet.graphics.OrderedGroup(0)
+Background = pyglet.graphics.OrderedGroup(1)
+BoardObj = pyglet.graphics.OrderedGroup(2)
+Foreground = pyglet.graphics.OrderedGroup(3)
+Players = pyglet.graphics.OrderedGroup(4)
+Walls = pyglet.graphics.OrderedGroup(5)
+Cursorobj = pyglet.graphics.OrderedGroup(6)
 
 class WallArray:
     def __init__(self,wall_img,shadow_wall_img):
@@ -17,47 +19,97 @@ class WallArray:
         self.img_wall=wall_img
         self.img_shadow_wall=shadow_wall_img
     
+    def command_manager(self,key):
+        """
+        DESIGNACION DE LOS COMANDOS PARA PAREDES
+        1   -> Enable_Shadow, Habilita el overlay para colocar una pared
+        2   -> Rotate_Wall, Rota la pared y actualiza su posicion
+        3   -> Place_Wall, Agrega la pared al arreglo existente y la dibuja
+        """
+        if key==1:
+            if self.enable_shadow():
+                print("Se habilito la sombra")
+        elif key==2:
+            if self.rotate_wall():
+                print("Se roto la sombra")
+        elif key==3:
+            if self.place_wall():
+                print("Se guardo pared")
+            else:
+                print("Guardado incorrecto")
 
-    def place_wall(self,Px,Py):
-        wall=pyglet.sprite.Sprite(  BOARD_BORDER+JUMP*Px,
-                                    BOARD_BORDER+JUMP*Py,
-                                    self.img_wall,
-                                    batch=Bruh,group=Walls)
-        wall.rotation=self.wall_shadow.rotation
-        self.wall_sprite_array.append(wall)
-
-    def delete_this(self):
-        self.wall_shadow.delete()
-        self.wall_shadow=False
+    def enable_shadow(self):
+        self.wall_shadow=pyglet.sprite.Sprite(self.img_shadow_wall,0,0,batch=Bruh,group=OutOfBounds)
+        self.wall_shadow.scale=SLOT_SCALE
 
     def rotate_wall(self):
         if self.wall_shadow.rotation==0:
             self.wall_shadow.rotation=90
-        else: 
+            if not self.refresh_shadow_pos():
+                self.wall_shadow.rotation=0
+                return False    
+        else:
             self.wall_shadow.rotation=0
-        self.refresh_shadow_pos()
+            if not self.refresh_shadow_pos():
+                self.wall_shadow.rotation=90
+                return False    
+        return True
 
-    def check_valid_placement(self,Bx,By):
-        if (self.wall_shadow.rotation==90):
-            node_coord_1=(Bx,By)
-            node_coord_1_end=(Bx,By+1)
-            node_coord_2=(Bx+1,By)
-            node_coord_2_end=(Bx+1,By+1)
-        else:
-            node_coord_1=(Bx,By)
-            node_coord_1_end=(Bx+1,By)
-            node_coord_2=(Bx,By+1)
-            node_coord_2_end=(Bx+1,By+1)
+    def place_wall(self):
+        if self.shadow_pos == (0,0):
+            return False
+        c1,c1_end,c2,c2_end=self.__create_coords(self.shadow_pos[0],self.shadow_pos[1])
+        self.save_wall(c1,c1_end)
+        self.save_wall(c2,c2_end)
+        for c in [c1,c2]:
+            wall=pyglet.sprite.Sprite(  self.img_wall,
+                                        BOARD_BORDER+JUMP*c[0],
+                                        BOARD_BORDER+JUMP*c[1],
+                                        batch=Bruh,group=Walls)
+            wall.scale=SLOT_SCALE
+            wall.rotation=self.wall_shadow.rotation
+            self.wall_sprite_array.append(wall)
+        self.wall_shadow.delete()
+        self.wall_shadow=False
+        self.shadow_pos=(0,0)
+        return True
 
-        if node_coord_1 not in self.wall_array:
-            return True
+
+    def save_wall(self,coord,coord_end):
+        if coord not in self.wall_array:
+            self.wall_array[coord]=[]
+        self.wall_array[coord].append(coord_end)
+        return True
+
+    def refresh_shadow_pos(self,*args):
+        """
+        Puede ser usada:
+        -Agregando la posicion X e Y del raton a los argumentos
+        -Sin argumentos (usara los de la ultima llamada con argumentos o (0,0)
+        """
+        if args:
+            self.last_mouse=args
+            Mx=int((args[0]-BOARD_BORDER)//JUMP)
+            My=int((args[1]-BOARD_BORDER)//JUMP)
         else:
-            if node_coord_2 not in self.wall_array[node_coord_1]:
+            Mx=int((self.last_mouse[0]-BOARD_BORDER)//JUMP)
+            My=int((self.last_mouse[1]-BOARD_BORDER)//JUMP)
+
+        if(self.wall_shadow):
+            pos=self.__fix_shadow_position(Mx,My)
+            if type(pos)!=bool:
+                if self.wall_shadow.group==OutOfBounds:
+                    self.wall_shadow.group=Cursorobj
+                self.wall_shadow.x=BOARD_BORDER+JUMP*pos[0]
+                self.wall_shadow.y=BOARD_BORDER+JUMP*pos[1]
+                self.shadow_pos=(pos[0],pos[1])
                 return True
-            else:
-                return False
+        return False
 
-    def fix_shadow_position(self,Mx,My):
+    def __fix_shadow_position(self,Mx,My):
+        """
+        AJUSTA A LA SOMBRA DENTRO DE LOS LIMITES PERMITIDOS Y EVITA COLOCAR SOBRE PAREDES
+        """
         if(self.wall_shadow.rotation==90):
             x_ini=0
             y_ini=1
@@ -70,33 +122,37 @@ class WallArray:
             y_limit=BOARD_SIZE-2
         Bx=min(x_limit,max(x_ini,Mx))
         By=min(y_limit,max(y_ini,My))
-        if self.check_valid_placement(Bx,By):
-            print("X: "+str(Bx)+", Y: "+str(By))
-            return True,Bx,By
+        if self.__check_valid_placement(Bx,By):
+            return Bx,By
+        else:
+            return False      
+
+    def __check_valid_placement(self,Bx,By):
+        c1,c1_end,c2,c2_end=self.__create_coords(Bx,By)
+        print([c1,c1_end,c2,c2_end])
+        print(self.wall_array)
+        if self.__check_array(c1,c1_end) and self.__check_array(c2,c2_end):
+            return True
         else:
             return False
 
-    def refresh_shadow_pos(self,*args):
-        if args:
-            self.last_mouse=args
-            Mx=int((args[0]-BOARD_BORDER)//JUMP)
-            My=int((args[1]-BOARD_BORDER)//JUMP)
+    def __check_array(self,coord,coord_end):
+        if coord in self.wall_array:
+            if coord_end in self.wall_array[coord]:
+                print("CONFLICTO")
+                return False
+        return True
+
+    def __create_coords(self,Bx,By):
+        """
+        GENERA LAS COORDENADAS DE AMBOS CUADROS PARA LA PARED
+        Coloca la menor coordenada a la derecha de cada tupla
+        Output: (coord1),(coord1_end),(coord2),(coord2_end)
+        """
+        if (self.wall_shadow.rotation==90):
+            return (Bx,By),(Bx,By-1),(Bx+1,By),(Bx+1,By-1)
         else:
-            Mx=int((self.last_mouse[0]-BOARD_BORDER)//JUMP)
-            My=int((self.last_mouse[1]-BOARD_BORDER)//JUMP)
-
-        if(self.wall_shadow):
-            #print("X: "+str(Mx)+", Y: "+str(My))
-            log=self.fix_shadow_position(Mx,My)
-            if log[0]:
-                self.wall_shadow.x=BOARD_BORDER+JUMP*log[1]
-                self.wall_shadow.y=BOARD_BORDER+JUMP*log[2]
-        return False
-        
-
-    def show_shadow_wall(self):
-        self.wall_shadow=pyglet.sprite.Sprite(self.img_shadow_wall,0,0,batch=Bruh,group=Walls)
-        self.wall_shadow.scale=SLOT_SCALE
+            return (Bx,By),(Bx-1,By),(Bx,By+1),(Bx-1,By+1)
 
 class Player:
     def __init__(self,initial_coords,goal_coords,color,AssCol,AssPath):
